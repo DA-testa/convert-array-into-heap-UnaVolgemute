b2c47a925d5c8b941cab09492b9e2a9e555ca904
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-03-16 17:19:51 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,7 +1,5 @@
 # python3
 
-import os
-
 def sift_down(i, data, swaps):
     min_index = i
     l = 2 * i + 1
@@ -25,33 +23,24 @@ def build_heap(data):
 def main():
     input_method = input("Enter 'I' to input from keyboard, 'F' to input from a file: ").strip().lower()
 
+    # get input data
     if input_method == 'i':
         n = int(input("Enter the length of the list: "))
         data = list(map(int, input("Enter the elements of the list separated by space: ").split()))
     elif input_method == 'f':
         file_path = input("Enter the path to the file containing the list: ")
-        if not os.path.exists(file_path):
-            print("File not found. Please try again.")
-            return
-        if os.path.getsize(file_path) == 0:
-            print("File is empty. Please try again.")
-            return
         with open(file_path, 'r') as f:
             n = int(f.readline().strip())
             data = list(map(int, f.readline().strip().split()))
     else:
         print("Invalid input method. Please try again.")
         return
-
     
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-   for d in data:
-        if not isinstance(d, int):
-            print("Invalid input data. Please try again.")
-            return
+   
 
     # input from keyboard
    
@@ -66,12 +55,7 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    num_swaps = len(swaps)
-    max_swaps = 4 * n
-    if num_swaps > max_swaps:
-        print("Too many swaps. Please try again.")
-        return
-    print("Number of swaps:", num_swaps)
+    assert len(swaps) <= 4 * n
 
     # output all swaps
     print(len(swaps))
