d654d33a93492aa82bb4c677a0301c51b3ba1442
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-03-16 16:29:03 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,46 +3,52 @@
 
 def build_heap(data):
     swaps = []
-    size = len(data)
-
-    for i in range((size // 2) - 1, -1, -1):
-        sift_down(data, i, size, swaps)
-
-    for i in range(size - 1, 0, -1):
+    n = len(data)
+    for i in range(n // 2 - 1, -1, -1):
+        swaps = sift_down(data, i, n, swaps)
+    for i in range(n - 1, 0, -1):
         data[0], data[i] = data[i], data[0]
         swaps.append((0, i))
-        sift_down(data, 0, i, swaps)
+        swaps = sift_down(data, 0, i, swaps)
     return swaps
 
 
-def sift_down(data, i, size, swaps):
-    max_index = i
-    left_child = (2 * i) + 1
-    right_child = (2 * i) + 2
-
-    if left_child < size and data[left_child] > data[max_index]:
-        max_index = left_child
-
-    if right_child < size and data[right_child] > data[max_index]:
-        max_index = right_child
-
-    if i != max_index:
-        data[i], data[max_index] = data[max_index], data[i]
-        swaps.append((i, max_index))
-        sift_down(data, max_index, size, swaps)
+def sift_down(data, i, n, swaps):
+    while 2 * i + 1 < n:
+        j = 2 * i + 1
+        if j + 1 < n and data[j + 1] > data[j]:
+            j += 1
+        if data[i] >= data[j]:
+            break
+        data[i], data[j] = data[j], data[i]
+        swaps.append((i, j))
+        i = j
+    return swaps
 
 
 def main():
+    data_input = input("Enter 'I' to input from keyboard, 'F' to input from a file: ")
+    if data_input == 'I':
+        n = int(input("Enter the length of the list: "))
+        data = list(map(int, input("Enter the list of integers: ").split()))
+    elif data_input == 'F':
+        filename = input("Enter the filename: ")
+        with open(filename, 'r') as f:
+            n = int(f.readline().strip())
+            data = list(map(int, f.readline().strip().split()))
+    else:
+        print("Invalid input.")
+        return
+    
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-    n = int(input())
-    data = list(map(int, input().split()))
+   
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+   
+
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -53,8 +59,11 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    assert len(swaps) < 4 * len(data)
-    print(len(swaps))
+    num_swaps = len(swaps)
+    max_swaps = 4 * n
+    print(f"Number of swaps: {num_swaps}")
+    if num_swaps <= max_swaps:
+        print("All swaps:")
 
     # output all swaps
     print(len(swaps))
