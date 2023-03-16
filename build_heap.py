2a0d024feef5b9bc298271b32c29745797f7577d
Una_Volgemute_221RDB287 <125603586+UnaVolgemute@users.noreply.github.com>
2023-03-16 16:10:19 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,17 +5,35 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    n = len(data)
+    swaps = []
+    for i in range(n//2, -1, -1):
+        sift_down(i, data, swaps)
 
     return swaps
 
+
+def sift_down(i, data, swaps):
+    n = len(data)
+    min_index = i
+    left_child = 2*i + 1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2*i + 2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(min_index, data, swaps)
 
 def main():
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    n = int(input())
+    data = list(map(int, input().split()))
 
     # input from keyboard
     n = int(input())
@@ -30,7 +48,8 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    assert len(swaps) < 4 * len(data)
+    print(len(swaps))
 
     # output all swaps
     print(len(swaps))
